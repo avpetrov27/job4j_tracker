9702e770d254ffd4f8b1ee11e317e06414be6ad6
avpetrov27 <avpetrov27@yandex.ru>
2023-03-21 17:12:17 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,7 +4,8 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item0 = new Item();
+        Item item0 = new Item("Заявка 1");
+        System.out.println(item0);
         System.out.println(item0.getCreated().format(DateTimeFormatter
                 .ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")));
     }
