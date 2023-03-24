e09461a9f46b0ec69f33d76189e9e8ae0b07ddec
avpetrov27 <avpetrov27@yandex.ru>
2023-03-24 11:23:20 +0300

    12.0. Написать тесты на StartUI. [#785 #403689]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -8,11 +8,7 @@ public class StartUITest {
     @Test
     public void whenCreateItem() {
         Input in = new StubInput(
-                new String[]{
-                        "0", "Item name1",
-                        "0", "Item name2",
-                        "0", "Item name3",
-                        "1"}
+                new String[]{"0", "Item name", "1"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
@@ -20,77 +16,37 @@ public class StartUITest {
                 new ExitAction()
         };
         new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name1");
-        assertThat(tracker.findAll()[1].getName()).isEqualTo("Item name2");
-        assertThat(tracker.findAll()[2].getName()).isEqualTo("Item name3");
-        assertThat(tracker.findAll().length).isEqualTo(3);
-        assertThat(tracker.findAll()[2].getId()).isEqualTo(3);
-    }
-
-    @Test
-    void showAllItems() {
+        assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
     }
 
     @Test
     public void whenReplaceItem() {
         Tracker tracker = new Tracker();
-        tracker.add(new Item("Item name1"));
-        tracker.add(new Item("Item name2"));
-        tracker.add(new Item("Item name3"));
+        Item item = tracker.add(new Item("Replaced item"));
+        String replacedName = "New item name";
         Input in = new StubInput(
-                new String[]{
-                        "0", "1", "New Item name1",
-                        "0", "3", "New Item name3",
-                        "0", "15", "New Item name15",
-                        "1"}
+                new String[]{"0", String.valueOf(item.getId()), replacedName, "1"}
         );
         UserAction[] actions = {
                 new EditAction(),
                 new ExitAction()
         };
         new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findById(1).getName()).isEqualTo("New Item name1");
-        assertThat(tracker.findById(2).getName()).isEqualTo("Item name2");
-        assertThat(tracker.findById(3).getName()).isEqualTo("New Item name3");
-        assertThat(tracker.findById(15)).isNull();
-        assertThat(tracker.findAll().length).isEqualTo(3);
-        assertThat(tracker.findAll()[2].getId()).isEqualTo(3);
+        assertThat(tracker.findById(item.getId()).getName()).isEqualTo(replacedName);
     }
 
     @Test
     public void whenDeleteItem() {
         Tracker tracker = new Tracker();
-        tracker.add(new Item("Item name1"));
-        tracker.add(new Item("Item name2"));
-        tracker.add(new Item("Item name3"));
+        Item item = tracker.add(new Item("Deleted item"));
         Input in = new StubInput(
-                new String[]{
-                        "0", "2",
-                        "0", "42",
-                        "1"}
+                new String[]{"0", String.valueOf(item.getId()), "1"}
         );
         UserAction[] actions = {
                 new DeleteAction(),
                 new ExitAction()
         };
         new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findById(2)).isNull();
-        assertThat(tracker.findById(42)).isNull();
-        assertThat(tracker.findById(1).getName()).isEqualTo("Item name1");
-        assertThat(tracker.findById(3).getName()).isEqualTo("Item name3");
-        assertThat(tracker.findAll().length).isEqualTo(2);
-        assertThat(tracker.findAll()[1].getId()).isEqualTo(3);
-        tracker.add(new Item("Item name4"));
-        assertThat(tracker.findAll().length).isEqualTo(3);
-        assertThat(tracker.findAll()[2].getId()).isEqualTo(4);
-        assertThat(tracker.findById(4).getName()).isEqualTo("Item name4");
-    }
-
-    @Test
-    void findItemById() {
-    }
-
-    @Test
-    void findItemByName() {
+        assertThat(tracker.findById(item.getId())).isNull();
     }
 }
