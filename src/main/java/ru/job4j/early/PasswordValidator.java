8aeb11dbe714498d9a2ad35237e9a5c28fcfefdc
avpetrov27 <avpetrov27@yandex.ru>
2023-03-29 17:59:31 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #403902]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -3,8 +3,8 @@ package ru.job4j.early;
 import static java.lang.Character.*;
 
 public class PasswordValidator {
-    private static final int MIN_LENGTH_PASSWORD = 8;
-    private static final int MAX_LENGTH_PASSWORD = 32;
+    private static final int MIN_LENGTH = 8;
+    private static final int MAX_LENGTH = 32;
     private static final String[] BAD_SUBSTRINGS = {
             "qwerty",
             "12345",
@@ -13,79 +13,58 @@ public class PasswordValidator {
             "user"
     };
 
-    private static final String[] SYMBOL_CHECKS_AND_E_MESSAGE = {
-            "Password should contain at least one uppercase letter",
-            "Password should contain at least one lowercase letter",
-            "Password should contain at least one figure",
-            "Password should contain at least one special symbol"
-    };
-
-    private static boolean isSymbolInGroup(char symbol, int type) {
-        return switch (type) {
-            case 0 -> isUpperCase(symbol);
-            case 1 -> isLowerCase(symbol);
-            case 2 -> isDigit(symbol);
-            case 3 -> !isLetterOrDigit(symbol);
-            default -> throw new IllegalStateException("Unexpected value: " + type);
-        };
-    }
-
-    private static boolean allChecksIsTrue(boolean[] checks) {
-        for (boolean check : checks) {
-            if (!check) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static void throwFirstException(boolean[] checks) {
-        for (int i = 0; i < checks.length; i++) {
-            if (!checks[i]) {
-                throw new IllegalArgumentException(SYMBOL_CHECKS_AND_E_MESSAGE[i]);
-            }
-        }
-    }
-
-    private static void existsMatchForAllGroups(String password) {
-        char[] chars = password.toCharArray();
-        boolean[] checks = new boolean[SYMBOL_CHECKS_AND_E_MESSAGE.length];
-        for (char aChar : chars) {
-            for (int i = 0; i < checks.length; i++) {
-                if (!checks[i]) {
-                    checks[i] = isSymbolInGroup(aChar, i);
-                }
-            }
-            if (allChecksIsTrue(checks)) {
-                return;
-            }
-        }
-        throwFirstException(checks);
-    }
-
-    private static boolean lengthBetween(String password) {
-        return password.length() >= MIN_LENGTH_PASSWORD && password.length() <= MAX_LENGTH_PASSWORD;
-    }
-
-    private static boolean existsBadSubstring(String password) {
-        for (String badSubstring : BAD_SUBSTRINGS) {
-            if (password.toUpperCase().contains(badSubstring.toUpperCase())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     public static String validate(String password) {
         if (password == null) {
-            throw new IllegalArgumentException("Password can't be null");
+            throw new IllegalArgumentException(
+                    "Password can't be null");
         }
-        if (!lengthBetween(password)) {
-            throw new IllegalArgumentException("Password should be length [" + MIN_LENGTH_PASSWORD + ", " + MAX_LENGTH_PASSWORD + "]");
+        if (password.length() < MIN_LENGTH || password.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(
+                    "Password should be length [" + MIN_LENGTH + ", " + MAX_LENGTH + "]");
         }
-        existsMatchForAllGroups(password);
-        if (existsBadSubstring(password)) {
-            throw new IllegalArgumentException("Password shouldn't contain substrings: " + String.join(", ", BAD_SUBSTRINGS));
+        boolean hasUpCase = false;
+        boolean hasLowCase = false;
+        boolean hasDigital = false;
+        boolean hasSpecial = false;
+        for (char aChar : password.toCharArray()) {
+            if (!hasUpCase) {
+                hasUpCase = isUpperCase(aChar);
+            }
+            if (!hasLowCase) {
+                hasLowCase = isLowerCase(aChar);
+            }
+            if (!hasDigital) {
+                hasDigital = isDigit(aChar);
+            }
+            if (!hasSpecial) {
+                hasSpecial = !isLetterOrDigit(aChar);
+            }
+            if (hasUpCase && hasLowCase && hasDigital && hasSpecial) {
+                break;
+            }
+        }
+        if (!hasUpCase) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one uppercase letter");
+        }
+        if (!hasLowCase) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one lowercase letter");
+        }
+        if (!hasDigital) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one figure");
+        }
+        if (!hasSpecial) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one special symbol");
+        }
+        for (String badSubstring : BAD_SUBSTRINGS) {
+            if (password.toUpperCase().contains(badSubstring.toUpperCase())) {
+                throw new IllegalArgumentException(
+                        "Password shouldn't contain substrings: "
+                                + String.join(", ", BAD_SUBSTRINGS));
+            }
         }
         return password;
     }
