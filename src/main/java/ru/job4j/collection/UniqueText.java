b7e5f41c7736203719c89c55919baa5843bf5883
avpetrov27 <avpetrov27@yandex.ru>
2023-04-02 18:38:23 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.Collections;
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        Collections.addAll(check, origin);
+        for (String s : text) {
+            if (!check.contains(s)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
