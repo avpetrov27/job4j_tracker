82bda8be8bd37e41ddaccc4e03084dff97fec4d1
avpetrov27 <avpetrov27@yandex.ru>
2023-04-03 18:26:21 +0300

    1. Паспорт и жители [#212656]

---
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PassportOffice {
+    private final Map<String, Citizen> citizens = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        if (!citizens.containsKey(citizen.getPassport())) {
+            citizens.put(citizen.getPassport(), citizen);
+            return true;
+        }
+        return false;
+    }
+
+    public Citizen get(String passport) {
+        return citizens.get(passport);
+    }
+}
